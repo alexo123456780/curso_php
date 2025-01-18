7e5b0accfff843113c7ad015712e0140f61feaba
alexo123456780 <alexitolope152004@gmail.com>
2025-01-17 22:39:04 -0600

    Archivo de practicas del dia 3

---
+++ index3.php
@@ -0,0 +1,23 @@
+<?php
+
+/*$fruits = 
+[
+    'Manzana' => 'Rojo',  'Pera' => 'Verde' , 'Sandia' => 'Rojo' , 'Uva' => 'Morado'
+];
+
+
+foreach($fruits as $key => $element){
+
+    echo "Fruta: $key "."Color: $element ". "<br><br>";
+
+}*/
+
+
+
+
+
+
+
+
+?>
+
