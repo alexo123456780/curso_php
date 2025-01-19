9efb3402b92e55e9a513a417210bb2ded1aada01
alexo123456780 <alexitolope152004@gmail.com>
2025-01-19 00:22:22 -0600

    Se agrego mas ejemplos de string

--- index6.php
+++ index6.php
@@ -29,13 +29,27 @@ $convertirMinisculas = function($texto){
 };
 
 
-$convertirMinisculas("HELLO BROTHER");
+$texto = 'Bienvenido a PHP';
+$texto2 = 'Programacion';
+$texto3 = 'Desarrollo Web';
+$texto4 = 'Full stack developer';
 
 
 
+$searchWord = fn($texto) => substr($texto,11);
+
+$searchWord2 = fn($texto2) => substr($texto2,0,3);
+
+$searchWord3 = fn($texto3) => substr($texto3,9);
+
+$searchWord4 = fn($texto4) => substr($texto4,4,6);
 
 
 
+echo "Resultado ".$searchWord($texto) ."<br><br>";
+echo "Resultado ".$searchWord2($texto2) ."<br><br>";
+echo "Resultado ".$searchWord3($texto3) ."<br><br>";
+echo "Resultado ".$searchWord4($texto4) ."<br><br>";
 
 
 
