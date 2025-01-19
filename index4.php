1fe4646837cbc29244a55cbfa58912cfba2bc421
alexo123456780 <alexitolope152004@gmail.com>
2025-01-19 00:24:54 -0600

    Cambio

---
+++ index4.php
@@ -0,0 +1,25 @@
+<?php
+
+function sumar($a,$b){
+
+    return $a+$b;
+}
+
+function restar($a,$b){
+
+    return $a - $b;
+}
+
+function multiplicar($a,$b){
+
+    return $a * $b;
+}
+
+$suma = "sumar";
+
+echo "El resultado de la suma es:  " .$suma(10,100);
+
+
+
+
+?>
