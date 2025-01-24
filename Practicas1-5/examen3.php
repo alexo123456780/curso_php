28fd51ca1a2dd9dc0605b058e656eddf420da9ce
alexo123456780 <alexitolope152004@gmail.com>
2025-01-24 01:28:18 -0600

    examen parte3

---
+++ Practicas1-5/examen3.php
@@ -0,0 +1,84 @@
+<?php
+
+/*Task1
+
+$estudiante = 
+[
+    "   Nombre" => "Alex" , "Edad" => 20 , "Curso" => "Dsm" , "Nota" => 10
+
+];
+
+foreach($estudiante as $key => $valor){
+
+    echo "$key:  ".$valor."<br><br>";
+
+}*/
+
+
+
+/*Task2
+$numeros = [10,20,30,40,50];
+
+
+function funcionArray($numeros){
+
+
+    return array_map(fn($elemento) => $elemento * 2 ,$numeros);
+
+}
+
+function obtenerNumero($numeros){
+
+
+    return array_filter($numeros,fn($elemento) => $elemento > 25);
+
+}
+
+echo "Numeros mayores a 25 son :  ".implode(" - ",obtenerNumero($numeros));*/
+
+
+//Task 3
+
+$productos = 
+[
+    "producto1" => ["nombre" => "licuadora" , "precio" => 500 , "stok" => 4],
+
+    "producto2" => ["nombre" => "lavadora" , "precio" => 10000, "stok" => 7],
+
+    "producto3" => ["nombre" => "laptop" , "precio" => 30000, "stok" => 10]
+
+];
+
+function incrementarPrecios($productos){
+
+    $incremento = array_map(function($elemento){
+
+        $elemento["precio"] += $elemento["precio"] * 0.1;
+
+        return $elemento;
+
+    },$productos);
+
+
+    foreach($incremento as $key => $valor){
+
+        echo "Nombre:  ".$valor["nombre"]." Precio:  ".$valor["precio"]."  Stock:  ".$valor["stok"]."<br><br>";
+
+    }
+
+}
+
+
+incrementarPrecios($productos);
+
+
+
+
+
+
+
+
+
+
+
+?>
